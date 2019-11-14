663c8956c338cb289e9d42aac553fa439d8d7057
Mahmoud Selim <mahmoud.selim@sypron.com>
2019-11-14 15:13:07 +0200

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     const location = search.value
-    fetch('http://localhost:3000/weather?address=' + location).then(res => {
+    fetch('/weather?address=' + location).then(res => {
         messageOne.textContent = ''
         res.json().then(data => {
             if(data.error){
